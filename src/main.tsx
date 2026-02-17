dde99257519fa1292d00775d7d965a38159adfcb
Danilo0617 <111926714+Danilo0617@users.noreply.github.com>
2026-02-16 18:53:41 -0600

    Simplify main.tsx by removing BrowserRouter
    Removed BrowserRouter and its related import.

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,11 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom'; // <--- Importamos el Router
 import App from './App.tsx';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <BrowserRouter basename="/inventario"> {/* <--- Agregamos el basename aquí */}
-      <App />
-    </BrowserRouter>
-  </StrictMode>,
+  <StrictMode>
+    <App />
+  </StrictMode>,
 );
+
