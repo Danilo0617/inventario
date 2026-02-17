4c2482464d17b2ff0281226dfff2c207bb80ae2a
Danilo0617 <111926714+Danilo0617@users.noreply.github.com>
2026-02-16 18:38:19 -0600

    Set base path for Vite configuration
    Added base path for deployment.

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  
+  base: '/inventario/', // <--- Agrega esta línea con el nombre de tu repositorio
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
