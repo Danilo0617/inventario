6a88a5cc4d83d6c636c7fd1e2a70f9c7dbcec046
Danilo0617 <111926714+Danilo0617@users.noreply.github.com>
2026-02-16 18:50:11 -0600

    Integrate BrowserRouter for routing support
    Added BrowserRouter to enable routing with a basename.

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom'; // <--- Importamos el Router
 import App from './App.tsx';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter basename="/inventario"> {/* <--- Agregamos el basename aquí */}
+      <App />
+    </BrowserRouter>
   </StrictMode>,
 );
